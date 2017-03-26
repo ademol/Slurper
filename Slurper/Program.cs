b2f8c7a7a0ec9753fed43ded715c32de728a2c72
AdM <arjan.de.mol@gmail.com>
2017-03-26 10:27:31 +0200

    * create intermediate dirs (instead of long filenames) * additional comments & some restructuring

--- Slurper/Program.cs
+++ Slurper/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-//using System.IO;
 using System.Text.RegularExpressions;
 using System.Collections;
 using Alphaleonis.Win32.Filesystem;
@@ -8,39 +7,45 @@ namespace Slurper
 {
     class Program
     {
+        /*
+         * Sluper: Utility to search for files on a Windows computer that match one or more regex patterns. 
+         *         The files found are then copied to a subdirectory in the location from where the program is run.
+         *         
+         *         note: 
+         *         The drive that the program is run from, is excluded from searching.
+         *           => suggested use is to run this program from an portable location (USB/HD) 
+         *           
+         */
 
-        private static Boolean Debug = false;
-        private static String cfgFileName = "slurper.cfg";
-        private static string ripDir = "rip";
-        private static String targetDirPath;
+        private static Boolean VERBOSE = false;                                 // show additional output what is done
+        private static Boolean DRYRUN = false;                                  // (only) show what will be done (has implicit VERBOSE)
+        private static string ripDir = "rip";                                   // relative root directory for files to be copied to
+        private static String cfgFileName = "slurper.cfg";                      // regex pattern(s) configuration file
+        private static String targetDirBasePath;                                // relative directory for file to be copied to
 
         private static char pathSep = Path.DirectorySeparatorChar;
-        private static string DefaultRegexPattern = @"(?i).*\.jpg";
+        private static string DefaultRegexPattern = @"(?i).*\.jpg";             // the default pattern that is used to search for jpg files
 
-        private static ArrayList filePatternsTolookfor = new ArrayList();
-        private static ArrayList drivesToSearch = new ArrayList();
+        private static ArrayList filePatternsTolookfor = new ArrayList();       // patterns to search  
+        private static ArrayList drivesToSearch = new ArrayList();              // drives to search (excludes the drive that the program is run from..)
 
-        private static ArrayList filesRipped = new ArrayList();
+        private static ArrayList filesRipped = new ArrayList();                 // files grabbed, to prevent multiple copies (in case of multiple matching patterns)
 
         private static char[] spinChars = new char[] { '|', '/', '-', '\\' };
         private static int spinCharIdx = 0;
-        
+
         static void Main(string[] args)
         {
-            //todo: more elegant handeling of parameters
-            // help ?
-            if (args.Length > 0 && args[0].Equals("/h")) { help(); }
+            // handle arguments
+            Arguments(args);
 
-            // Debug ?
-            if (args.Length > 0 && args[0].Equals("/d")) { Debug = true; }
-
-            // create target directory
+            // determine & create target directory
             CreateTargetLocation();
 
             // configuration 
             Configure();
 
-            // drives
+            // get drives to search
             GetDriveInfo();
 
             // find files matching pattern(s) from all applicable drives, and copy them to ripdir
@@ -48,16 +53,29 @@ namespace Slurper
 
         }
 
+        static void Arguments(string[] args)
+        {
+            //todo: more elegant handeling of parameters  and allow for multiple param selection....
+            // help ?
+            if (args.Length > 0 && args[0].Equals("/h")) { help(); }
+
+            // Verbose ?
+            if (args.Length > 0 && args[0].Equals("/v")) { VERBOSE = true; }
+
+            // dryrun
+            if (args.Length > 0 && args[0].Equals("/d")) { DRYRUN = true; VERBOSE = true; }
+        }
 
         static void help()
         {
             //todo: nicer help
             String txt = "Copy files that have their filename matched, to ./rip/<hostname><timestamp> directory \n\n";
             txt += "In default mode (without cfg file) it matches jpg files with regex \n(i?).*\\\\.jpg\n\n";
-            txt += "use the /d flag for verbose output => slurper.exe /d \n";
+            txt += "use the /v flag for verbose output => slurper.exe /v \n";
+            txt += "use the /d flag for dryrun + verbose output (no filecopy mode) => slurper.exe /d      \n";
             txt += "\n";
             txt += "(optional) uses a configfile (./slurper.cfg) to specify custom regexes to match \n";
-    
+
             Console.WriteLine(txt);
             Environment.Exit(0);
         }
@@ -65,20 +83,16 @@ namespace Slurper
         static void spin()
         {
             // fold back to begin char when needed
-            if ( spinCharIdx + 1 == spinChars.Length )
-            {
-                spinCharIdx = 0;
-            } else
-            {
-                spinCharIdx++;
-            }
+            if (spinCharIdx + 1 == spinChars.Length)
+            { spinCharIdx = 0; }
+            else
+            { spinCharIdx++; }
 
             char spinChar = spinChars[spinCharIdx];
 
-            //set the spinner position to the current console position
-            //Console.CursorLeft = Console.CursorLeft > 0 ? Console.CursorLeft - 1 : 0;
+            //set the spinner position
             Console.CursorLeft = 0;
-                
+
             //write the new character to the console
             Console.Write(spinChar);
         }
@@ -122,7 +136,6 @@ namespace Slurper
 
         }
 
-
         static String[] getFiles(string dir)
         {
 
@@ -160,18 +173,28 @@ namespace Slurper
                 filesRipped.Add(filename);
 
                 // determine target filename
-                String targetFileName = filename;
+                String targetFileName = Path.GetFileName(filename);
+                String targetRelativePath = Path.GetDirectoryName(filename);
 
-                targetFileName = targetFileName.Replace('\\', '_');
-                targetFileName = targetFileName.Replace(':', '_');
 
-                targetFileName = targetDirPath + pathSep + targetFileName;
-                 
-                if (Debug) { Console.WriteLine("ripping [{0}] => [{1}]", filename, targetFileName); }
+                targetRelativePath = targetRelativePath.Replace(':', '_');
+                String targetPath = targetDirBasePath + pathSep + targetRelativePath + pathSep;
+
+
+                String targetFileNameFullPath = targetPath + targetFileName;
+
+                if (VERBOSE) { Console.WriteLine("ripping [{0}] => [{1}]", filename, targetFileNameFullPath); }
                 try
                 {
-                    File.Copy(filename, targetFileName);
-          
+                    // do the filecopy unless this is a dryrun
+                    if (!DRYRUN)
+                    {
+                        Directory.CreateDirectory(targetPath);
+                        File.Copy(filename, targetFileNameFullPath);
+                    }
+
+
+
                 }
                 catch (Exception e)
                 {
@@ -180,7 +203,6 @@ namespace Slurper
             }
 
         }
-
 
         static void GetDriveInfo()
         {
@@ -191,18 +213,18 @@ namespace Slurper
 
             // mydrive
             String mydrive = Path.GetPathRoot(Directory.GetCurrentDirectory());
-            if (Debug) { Console.WriteLine("mydrive = [{0}]", mydrive); }
+            if (VERBOSE) { Console.WriteLine("mydrive = [{0}]", mydrive); }
 
             foreach (DriveInfo d in allDrives)
             {
                 // skip the drive i'm running from
                 if ((mydrive.ToUpper()).Equals(d.Name.ToUpper()))
                 {
-                    if (Debug) { Console.WriteLine("skipping mydrive [{0}]", mydrive); }
+                    if (VERBOSE) { Console.WriteLine("skipping mydrive [{0}]", mydrive); }
                     continue;
                 }
 
-                if (Debug) { Console.WriteLine(d.Name); }
+                if (VERBOSE) { Console.WriteLine(d.Name); }
                 drivesToSearch.Add(d.Name);
             }
 
@@ -213,20 +235,19 @@ namespace Slurper
             String curDir = Directory.GetCurrentDirectory();
             String hostname = (System.Environment.MachineName).ToLower();
             String dateTime = String.Format("{0:yyyyMMdd_hh-mm-ss}", DateTime.Now);
-            if (Debug) { Console.WriteLine("[{0}][{1}][{2}]", hostname, curDir, dateTime); }
+            if (VERBOSE) { Console.WriteLine("[{0}][{1}][{2}]", hostname, curDir, dateTime); }
 
-            targetDirPath = string.Concat(curDir, pathSep, ripDir, pathSep, hostname, "_", dateTime);
-            if (Debug) { Console.WriteLine(targetDirPath); }
+            targetDirBasePath = string.Concat(curDir, pathSep, ripDir, pathSep, hostname, "_", dateTime);
+            if (VERBOSE) { Console.WriteLine(targetDirBasePath); }
 
             try
             {
-                Directory.CreateDirectory(targetDirPath);
-
+                if (!DRYRUN) { Directory.CreateDirectory(targetDirBasePath); }
 
             }
             catch (Exception e)
             {
-                Console.WriteLine("failed to create director [{0}][{1}]", targetDirPath, e.Message);
+                Console.WriteLine("failed to create director [{0}][{1}]", targetDirBasePath, e.Message);
 
             }
         }
@@ -236,7 +257,7 @@ namespace Slurper
             if (!LoadConfigFile())
             {
                 // default config
-                if (Debug) { Console.WriteLine("config file [{0}] not found => using defaults", cfgFileName); }
+                if (VERBOSE) { Console.WriteLine("config file [{0}] not found => using defaults", cfgFileName); }
 
                 // add a regex set as a default.
                 filePatternsTolookfor.Add(DefaultRegexPattern);
@@ -253,7 +274,7 @@ namespace Slurper
                 Regex r = new Regex(REGEXpattern);
                 try
                 {
-                    //todo: also move to http://alphafs.alphaleonis.com/  ?
+                    //todo: also move to alphafs ?
                     using (System.IO.StreamReader sr = new System.IO.StreamReader(cfgFileName))
                     {
                         while (!sr.EndOfStream)
@@ -266,11 +287,11 @@ namespace Slurper
                             {
                                 String regex = m.Groups[0].Value;
                                 filePatternsTolookfor.Add(regex);
-                                if (Debug) { Console.WriteLine("[{0}] => regex:[{1}]", line, regex); }
+                                if (VERBOSE) { Console.WriteLine("[{0}] => regex:[{1}]", line, regex); }
                             }
                             else
                             {
-                                if (Debug) { Console.WriteLine("[{0}] => regex:[{1}]", line, "---skipped---"); }
+                                if (VERBOSE) { Console.WriteLine("[{0}] => regex:[{1}]", line, "---skipped---"); }
                             }
                         }
                     }
