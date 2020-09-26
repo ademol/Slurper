a6d86e18c08c011963b12db7dabbefa5301a6191
Arjan de Mol <ademol@users.noreply.github.com>
2020-09-26 22:28:53 +0200

    Ignore exceptions in spinner (not Thread safe)

--- Slurper/Output/Spinner.cs
+++ Slurper/Output/Spinner.cs
@@ -9,19 +9,26 @@ namespace Slurper.Output
 
         public static void Spin()
         {
-            // fold back to begin char when needed
-            if (_spinCharIdx + 1 == SpinChars.Length)
+            try
+            {
+                // fold back to begin char when needed
+                if (_spinCharIdx + 1 == SpinChars.Length)
+                    _spinCharIdx = 0;
+                else
+                    _spinCharIdx++;
+
+                var spinChar = SpinChars[_spinCharIdx];
+
+                //set the spinner position
+                Console.CursorLeft = 0;
+
+                //write the new character to the console
+                Console.Write(spinChar);
+            }
+            catch (Exception e)
+            {
                 _spinCharIdx = 0;
-            else
-                _spinCharIdx++;
-
-            var spinChar = SpinChars[_spinCharIdx];
-
-            //set the spinner position
-            Console.CursorLeft = 0;
-
-            //write the new character to the console
-            Console.Write(spinChar);
+            }
         }
     }
 }
